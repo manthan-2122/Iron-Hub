a3367357bda938dfdd3a5029e1eb7879d8b2c406
Hetavin Pokiya <hetavinpokiya4672@gmail.com>
2026-07-09 11:56:18 +0530

    Update database password in db_connection function

--- localhost.py
+++ localhost.py
@@ -4,10 +4,10 @@ def db_connection():
     conn = pymysql.connect(
         host="mysql-1e100372-developer1-e1bc.g.aivencloud.com",
         user="avnadmin",
-        password="patel_pmp@0304",
+        password="AVNS_7MCekzuveaCeqc69phB",
         database="gym_management",
         port=27075,
         ssl={"ssl": {}},
         cursorclass=pymysql.cursors.DictCursor
-        )
-    return conn 
+    )
+    return conn
